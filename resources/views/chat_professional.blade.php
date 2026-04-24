cfeeb084e3fd9c645a5e51be92da65c21c889e82
Monu <your-email@example.com>
2026-04-24 16:17:56 +0530

    update

--- resources/views/chat_professional.blade.php
+++ resources/views/chat_professional.blade.php
@@ -36,7 +36,7 @@
         .page {
             min-height: 100vh;
             display: flex;
-            align-items: center;
+            align-items: center;    
             justify-content: center;
             padding: 32px;
         }
